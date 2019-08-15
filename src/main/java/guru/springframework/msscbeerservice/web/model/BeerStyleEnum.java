0297c57dcb46a9f176c85089b2090576be99df6f
Ziru2k44 <bernardo.b.lourenco@gmail.com>
2019-08-15 20:17:49 +0100

    adding web data. Closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by Bernardo on 8/15/2019
+ */
+public enum BeerStyleEnum {
+    LARGE, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
